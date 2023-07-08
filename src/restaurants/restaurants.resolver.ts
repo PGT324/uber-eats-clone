532a17934dcfee715248dc7bbfd5a97043ed8886
박건태 <qkrrjsxo456@naver.com>
2023-07-08 17:16:49 +0900

    graphql dto

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,18 @@
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+
+@Resolver()
+export class RestaurantResolver {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
+  }
+
+  @Mutation(() => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
+  }
+}
